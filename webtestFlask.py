98412b89056e0851d33f989b9bbd08661ef692f1
Evan <varghese.evan@gmail.com>
2018-02-04 14:46:12 -0600

    testing basic web usability

--- webtestFlask.py
+++ webtestFlask.py
@@ -4,8 +4,33 @@
 
 from flask import Flask
 from flask import render_template
+from Adafruit_PWM_Servo_Driver import PWM
+import time
 
 app = Flask(__name__)
+
+# Initialise the PWM device using the default address
+pwm = PWM(0x40)
+# Note if you'd like more debug output you can instead run:
+# pwm = PWM(0x40, debug=True)
+
+servoMin = 150  # Min pulse length out of 4096
+servoMax = 600  # Max pulse length out of 4096
+
+
+def setServoPulse(channel, pulse):
+    # initializes the frequencies and currents for the servo
+    pulseLength = 1000000                   # 1,000,000 us per second
+    pulseLength /= 60                       # 60 Hz
+    print "%d us per period" % pulseLength
+    pulseLength /= 4096                     # 12 bits of resolution
+    print "%d us per bit" % pulseLength
+    pulse *= 1000
+    pulse /= pulseLength
+    pwm.setPWM(channel, 0, pulse)
+
+
+pwm.setPWMFreq(60)
 
 
 @app.route('/')
@@ -22,6 +47,57 @@ def about():
 @app.route('/index.html')
 def index():
     return render_template('index.html')
+
+
+@app.route('/routin0.html')
+def routine0():
+    return render_template('routine0.html')
+    pwm.setPWM(0, 0, servoMin)
+    pwm.setPWM(2, 0, servoMin)
+    time.sleep(1)
+    pwm.setPWM(0, 0, 375)
+    pwm.setPWM(2, 0, 375)
+    time.sleep(1)
+    pwm.setPWM(0, 0, servoMax)
+    pwm.setPWM(2, 0, servoMax)
+    time.sleep(1)
+    pwm.setPWM(0, 0, servoMin)
+    pwm.setPWM(2, 0, servoMin)
+    time.sleep(1)
+
+
+@app.route('/routine1.html')
+def routine1():
+    return render_template('routine1.html')
+    pwm.setPWM(0, 0, servoMin)
+    pwm.setPWM(2, 0, servoMin)
+    time.sleep(1)
+    pwm.setPWM(0, 0, 375)
+    pwm.setPWM(2, 0, 375)
+    time.sleep(1)
+    pwm.setPWM(0, 0, servoMax)
+    pwm.setPWM(2, 0, servoMax)
+    time.sleep(1)
+    pwm.setPWM(0, 0, servoMin)
+    pwm.setPWM(2, 0, servoMin)
+    time.sleep(1)
+
+
+@app.route('/routine2.html')
+def routine2():
+    return render_template('routine2.html')
+    pwm.setPWM(0, 0, servoMin)
+    pwm.setPWM(2, 0, servoMin)
+    time.sleep(1)
+    pwm.setPWM(0, 0, 375)
+    pwm.setPWM(2, 0, 375)
+    time.sleep(1)
+    pwm.setPWM(0, 0, servoMax)
+    pwm.setPWM(2, 0, servoMax)
+    time.sleep(1)
+    pwm.setPWM(0, 0, servoMin)
+    pwm.setPWM(2, 0, servoMin)
+    time.sleep(1)
 
 
 if __name__ == "__main__":

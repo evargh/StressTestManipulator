d8e3e2939c5f624d171323fa10e1d8dd11a690b7
Evan <varghese.evan@gmail.com>
2018-02-04 09:35:02 -0600

    properly styled

--- webtestFlask.py
+++ webtestFlask.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python
 
 from flask import Flask
-
 from flask import render_template
+from tkinter import Tk, Label, Button
 
 app = Flask(__name__)
 
-@app.route('/')
 
+@app.route('/')
 def mypysite(name=None):
-  return render_template('index.html')
+    # return the html page i already made
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
-  app.run('0.0.0.0')
-
-
-
+    app.run('0.0.0.0')

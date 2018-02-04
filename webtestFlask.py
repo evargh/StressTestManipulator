704fcdd8317a961f16d29718eb5b399746eb5df9
evargh <varghese.evan@gmail.com>
2018-02-04 02:39:32 +0000

    nut

---
+++ webtestFlask.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from flask import Flask
+
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def mypysite(name=None):
+  return render_template('index.html')
+
+if __name__ == "__main__":
+  app.run('0.0.0.0')
+
+
+
